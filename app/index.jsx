a90816f5c6444181964c38b78eec5b0d6bca0df2
ukarsh2004git <utkarshshahare2004@gmail.com>
2024-11-25 15:21:00 +0530

    created landing page

---
+++ app/index.jsx
@@ -0,0 +1,35 @@
+import { useRouter } from "expo-router";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+export default function Page() {
+    const router = useRouter()
+  return (
+    <View style={styles.container}>
+        <TouchableOpacity onPress={()=>router.push("home")}>
+            <Text>Click</Text>
+        </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    padding: 24,
+  },
+  main: {
+    flex: 1,
+    justifyContent: "center",
+    maxWidth: 960,
+    marginHorizontal: "auto",
+  },
+  title: {
+    fontSize: 64,
+    fontWeight: "bold",
+  },
+  subtitle: {
+    fontSize: 36,
+    color: "#38434D",
+  },
+});
